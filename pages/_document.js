c89e6c0f7bf09224e25172323933cb2c9515bc69
fuglsetm <fuglsetm@gmail.com>
2024-10-15 09:48:28 +0000

    favicon #2

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       <body className="antialiased">
         <Main />
         <NextScript />
