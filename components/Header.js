f67d4bd6a44ddb4ecc487e6e159d99705ac638d7
fuglsetm <fuglsetm@gmail.com>
2024-12-05 17:43:35 +0000

    fonts_1

--- components/Header.js
+++ components/Header.js
@@ -2,17 +2,10 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="mb-8 text-lg text-gray-800">
-      <div className="flex items-center space-x-3">
-        <img 
-          src="/profile.jpg" 
-          alt="Profile Image" 
-          className="w-10 h-10 rounded-full"
-        />
-        <h1 className="movable-link text-lg text-gray-800">
+    <header className="mb-8 text-lg">
+        <p className="movable-link text-xl font-serif">
           <Link href="/">Martin Fuglset</Link>
-        </h1>
-      </div>
+        </p>
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,23 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        gray: {
-          50: '#f7f7f7',
-          100: '#e1e1e1',
-          200: '#cfcfcf',
-          300: '#b1b1b1',
-          400: '#9e9e9e',
-          500: '#7e7e7e',
-          600: '#626262',
-          700: '#515151',
-          800: '#3b3b3b',
-          900: '#222222',
-        },
+      fontFamily: {
+        sans: ['SuisseIntl', 'sans-serif'], // Default sans-serif font
+        serif: ['Mondwest', 'serif'],      // Optional serif font
       },
     },
   },
-  darkMode: false, // Sync with the system's dark mode preference
+  darkMode: false,
   plugins: [],
 };
-
