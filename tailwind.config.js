776673d499a517f1cec9d09c6f0b7742efd0b57e
Martin Fuglset <fuglsetm@gmail.com>
2025-01-27 22:00:06 +0100

    font reduction

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,18 @@ module.exports = {
       fontFamily: {
         sans: ['Roboto Mono', 'monospace'],
       },
+      fontSize: {
+        'xs': '0.75rem',
+        'sm': '0.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '3.75rem',
+      },
     },
   },
   plugins: [],
